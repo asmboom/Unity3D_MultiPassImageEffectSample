81e6bd11db6247af5e7a0971f49e55777ad2ddfd
umeda <umeda@team-lab.com>
2016-09-07 14:00:24 +0900

    RenderTexture を Temporary に変更。

--- Assets/MultiPassShader.cs
+++ Assets/MultiPassShader.cs
@@ -14,8 +14,14 @@ namespace XJ.Unity3D.ImageEffects
             // renderTexture から null ( = Screen) へ描画するときに Pass 1 を使う。
             // Blit 関数の引数 dest が null のとき、スクリーンに描画される。
 
-            RenderTexture renderTexture = new RenderTexture
+            // 複数パスを用いた描画のために一時的に RenderTexture を利用する場合、
+            // new RenderTexture よりも RenderTexture.GetTemporary を用いる方が良いようです。
+            
+            RenderTexture renderTexture = RenderTexture.GetTemporary
                 (source.width, source.height, source.depth, source.format);
+
+            //RenderTexture renderTexture = new RenderTexture
+            //    (source.width, source.height, source.depth, source.format);
 
             Graphics.Blit(source, renderTexture, base.Material, 0);
             Graphics.Blit(renderTexture, null, base.Material, 1);
